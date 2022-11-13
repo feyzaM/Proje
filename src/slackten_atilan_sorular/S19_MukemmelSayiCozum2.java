0c07530e2d7be21a51e6d80721ba8860c4d0829f
feyza <feyzamagden@gmail.com>
2022-11-13 20:23:56 +0300

    mukemmel sayi cozum 2

--- src/slackten_atilan_sorular/S19_MukemmelSayiCozum2.java
+++ src/slackten_atilan_sorular/S19_MukemmelSayiCozum2.java
@@ -2,28 +2,37 @@ package slackten_atilan_sorular;
 
 public class S19_MukemmelSayiCozum2 {
     public static void main(String[] args) {
-        int sayi=16;
-        int toplam=0;
 
-        //i===> bolen
-        //j==> bolum
+        /*
+        Sayinin Mükemmel sayı olup olmadığını kontrol eden Java kodunu yazın.
+        //Sayının kendisi hariç pozitif bölenlerinin toplamı o sayıya eşitse,
+        //herhangi bir sayı Mükemmel Sayı olarak bilinir.
+        //Input :6
+        //Output:6 Mukemmel Sayidir.
+        //======================
+        //Input7
+        //Output:   7 Mukemmel Sayidir degildir.
+         */
 
-        for (int i = 1; i <=sayi/2 ; i++) {
-            for (int j = 1; j <=sayi ; j++) {
-                if (i*j==sayi){
-                    toplam+=i;
-                    System.out.println(i);
+       int input=6;
+       int tamBolenToplam=0;
+       //i==>bolen
+        //j==>bolum
+
+        for (int i = 1; i <=input/2 ; i++) {
+            for (int j = 1; j <=input ; j++) {
+                if (i*j==input){
+                    tamBolenToplam+=i;
+                    System.out.println(i + " bir tam bolendir");
                 }
             }
         }
-        if (sayi==toplam){
-            System.out.println(sayi + " mukemmel sayidir");
-        }else{
-            System.out.println(sayi + " mukemmel sayi degildir");
-        }
+         if (input==tamBolenToplam){
+             System.out.println(input + " mukemmel sayidir");
+         }else{
+             System.out.println(input + " mukemmel sayi degildir");
+         }
+
+
     }
-
-
-
-
 }
